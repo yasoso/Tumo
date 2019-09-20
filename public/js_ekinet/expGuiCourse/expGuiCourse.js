2f088c1ae105f2942a3b7c8f7adf2eddc87adb8d
yukineko08 <yukiko19960829@yahoo.co.jp>
2019-09-20 09:10:51 +0900

    chnge image by 乗車数

--- public/js_ekinet/expGuiCourse/expGuiCourse.js
+++ public/js_ekinet/expGuiCourse/expGuiCourse.js
@@ -2172,7 +2172,12 @@ var expGuiCourse = function (pObject, config) {
                     } else {
                         buffer += '<div class="exp_fare">';
                     }
-                    buffer += '<div class="exp_bar"></div>';
+                    alert(Number(getTextValue(fareList[0].Oneway)));
+                    if(Number(getTextValue(fareList[0].Oneway))>1000){
+                        buffer += '<div class="exp_bar2"></div>';
+                    }else{
+                        buffer += '<div class="exp_bar"></div>';
+                    }
                     //buffer += '<div class="exp_end"></div>';
                     buffer += '</div>';
                     buffer += '</div>';
